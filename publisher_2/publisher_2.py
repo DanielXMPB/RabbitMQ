2c1eac93884d4666ad4552507e6f4fee98c3456f
DanielXMPB <106484512+DanielXMPB@users.noreply.github.com>
2023-06-23 11:48:19 -0500

    Prueba RabbitMQ

---
+++ publisher_2/publisher_2.py
@@ -0,0 +1,13 @@
+import pika, sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='192.168.20.27',
+    credentials=pika.PlainCredentials("daniel", "1234")))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "info: Noticias de deportes"
+channel.basic_publish(exchange='logs', routing_key='', body=message)
+print(" [x] Sent %r" % message)
+connection.close()
---
+++ subscriber_2/subscriber_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "192.168.20.27", UserName = "daniel", Password = "1234"};
+
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange:"logs", type:ExchangeType.Fanout);
+
+                    var queuName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue:queuName, exchange:"logs", routingKey:"");
+
+                    Console.WriteLine("Waiting for logs...");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine($"[x] Received {message}");
+                    };
+
+                    channel.BasicConsume(queue:queuName, autoAck:true, consumer:consumer);
+
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
