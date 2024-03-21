0a46f7fcf5883848c01698b1c5f377b45c7c9c3b
Daniel Mauricio Pérez Bolívar <106484512+DanielXMPB@users.noreply.github.com>
2024-03-20 19:45:03 -0500

    Correcion version dotnet

--- publisher_3/obj/Debug/net8.0/publisher_3.AssemblyInfo.cs
+++ publisher_3/obj/Debug/net8.0/publisher_3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("subscriber_2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("publisher_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084ffc7f2851c519758121bc39baa3dc15a2b8d5")]
-[assembly: System.Reflection.AssemblyProductAttribute("subscriber_2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("subscriber_2")]
+[assembly: System.Reflection.AssemblyProductAttribute("publisher_3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("publisher_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
